c1a346b90cb85e0036b1b375ea1c9788b5a14bc0
chebbimohamedali <chebbi.mohamed.ali90@gmail.com>
2023-05-09 18:39:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Forms from './Forms';
+import Text from './Text';
+
+function App() {
+  return (
+    <div className="App">
+      <Text />   
+      <Forms /> 
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Text.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Text() {
+  return (
+    <div>
+        <marquee>
+            welcome ! Contact us !
+        </marquee>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Text
